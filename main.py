d47e85d52b456cd5349db85a8c2a48072c0b402f
CyberGeni <70205370+CyberGeni@users.noreply.github.com>
2022-06-01 02:41:02 +0100

    i'm creating the single rounds game part

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-# nothing to see here because this user didn't sleep during the night and needs to sleep
+import random
+
+choices = ['R', 'P', 'S']
+
+def computerPlay(): 
+    print(random.choice(choices))
+
+def playRound(playerSelection, computerSelection):
+    if playerSelection == 'R' and computerSelection == 'S':
+        print ('You win, Rock beats Scissors')
+    elif playerSelection == 'R' and computerSelection == 'P':
+        print ('You lose, Paper beats Rock')
+    elif playerSelection == 'P' and computerSelection == 'R':
+        print ('You win, Paper beats Rock')
+    elif playerSelection == 'P' and computerSelection == 'S':
+        print ('You lose, Scissors beats Paper')
+    elif playerSelection == 'S' and computerSelection == 'P':
+        print ('You win, Scissors beats Paper')
+    elif playerSelection == 'S' and computerSelection == 'R':
+        print ('You lose, Rock beats Scissors')
+    else:
+        print ('Invalid input, try again')
+
+playerSelection = str(input('Rock, Paper, or Scissors? (R for Rock, P for Paper, S for Scissors) '))
+computerSelection = computerPlay()
+
+print(playRound(playerSelection, computerSelection))
