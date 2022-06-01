95655e543120e807668b6629b7db57d67d6e899e
CyberGeni <70205370+CyberGeni@users.noreply.github.com>
2022-06-01 18:43:10 +0100

    succesfully implemented 5times loop

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import random
 
-choices = ['R', 'P', 'S']
+choices = ['R', #for rock
+           'P', #for paper
+           'S'  #for scissors
+           ]  
 
 def computerPlay():
     computerSelection = random.choice(choices)
@@ -8,33 +11,48 @@ def computerPlay():
 
 def playRound(playerSelection, computerSelection):
     if (playerSelection == 'R') and computerSelection == 'S':
+        print('You played Rock and Computer played Scissors')
         print('You win, Rock beats Scissors')
     elif playerSelection == 'R' and computerSelection == 'P':
+        print('You played Rock and Computer played Paper')
         print('You lose, Paper beats Rock')
     elif playerSelection == 'P' and computerSelection == 'R':
+        print('You played Paper and Computer played Rock')
         print ('You win, Paper beats Rock')
     elif playerSelection == 'P' and computerSelection == 'S':
+        print('You played Paper and Computer played Scissors')
         print ('You lose, Scissors beats Paper')
     elif playerSelection == 'S' and computerSelection == 'P':
+        print('You played Scissors and Computer played Paper')
         print ('You win, Scissors beats Paper')
     elif playerSelection == 'S' and computerSelection == 'R':
+        print('You played Scissors and Computer played Rock')
         print ('You lose, Rock beats Scissors')
     elif playerSelection == 'R' and computerSelection == 'R':
-        print('Tie')
+        print('You played Rock and Computer also played Rock')
+        print('It\'s a Tie!')
     elif playerSelection == 'P' and computerSelection == 'P':
-        print('Tie')
+        print('You played Paper and Computer also played Paper')
+        print('It\'s a Tie')
     elif playerSelection == 'S' and computerSelection == 'S':
-        print('Tie')
+        print('You played Scissors and Computer also played Scissors')
+        print('It\'s a Tie')
     else:
         print('Invalid input')
 
-playerSelection = str(input('Rock, paper or scissors?'))
-computerSelection = computerPlay()
+# values for the gameplay
+def gameplay():
+    global playerSelection, computerSelection
+    playerSelection = str(input('Rock, Paper or Scissors? (R for Rock, P for Paper, S for Scissors)'))
+    computerSelection = computerPlay()
+    playRound(playerSelection, computerSelection)
 
-if computerSelection == 'R':
-    print('Computer played Rock')
-elif computerSelection == 'P':
-    print('Computer played Paper')
-else:
-    print('Computer played Scissors')
+rounds = 5
+score_count = 0
 
+def game():
+    for i in range(rounds):
+        i += 1
+        gameplay()  
+
+game()
